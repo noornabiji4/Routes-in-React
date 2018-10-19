8c9d2e35cfac345116db4d03379a9e3840febdca
Noor <noornabiji4@gmail.com>
2018-10-19 14:46:52 +0530

    navbar file

---
+++ src/nav.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+
+
+// class Nav extends Component { 
+
+//     render(){
+      
+//         return(
+//             <div className="navbar">
+                
+//             </div>
+//         )
+//     }
+// }
+
+// export default Nav
